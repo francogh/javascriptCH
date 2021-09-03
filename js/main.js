00fa6989c9d90a7c0acf8d897497accf12ea5417
Franco Ghiotti <francoghiotti@gmail.com>
2021-09-03 19:38:32 -0300

    mejoramos el entregable nº2

--- js/main.js
+++ js/main.js
@@ -23,23 +23,29 @@
 alert('Hola, este sistema podra decirte si puede jubilarte');
 let nombre = prompt('Cual es tu nombre?');
 alert('Hola ' + ' ' + nombre);
-let edad = prompt (nombre + ' '+ 'cual es tu edad?');
+let edad = parseInt ( prompt (nombre + ' '+ 'cual es tu edad?'));
 let sexo = prompt ('Por favor coloca tu sexo \nM para masculino \nF para femenino');
 
 
-if (nombre != '' && edad != '' && sexo != '')  {
-    if (((edad >= 60) && (sexo == 'F' || 'f') ) || ((edad >= 65) && (sexo == 'M' || 'm'))){
-        if (sexo = 'M') {
-            alert('Puedes jubilarte por ser mujer y tener 60 años o mas')
+if ((nombre != '') && (!Number.isNaN(edad)) && (sexo != ''))  {
+    if ((edad >= 60 && (sexo == 'F' || sexo == 'f')) || (edad >= 65 && (sexo == 'M' || sexo == 'm') )) {
+        if (sexo == 'M' || sexo == 'm' ) {
+            alert('Puedes jubilarte por ser hombre y tener 60 años o mas')
         }else{
-            alert('Puedes jubilarte por ser hombre y tener 65 años o mas')
+            alert('Puedes jubilarte por ser mujer y tener 65 años o mas')
         }
     
+    }else if (sexo == 'f' || sexo == 'F' ) {
+        alert('No cumples con la edad necesaria, aún te faltan' + ' ' +(60 - edad) + ' ' + 'años para poder jubilarte' );
+        console.log('Sigue laburando');
+    }else if (sexo == 'm' || sexo == 'M' ) {
+        
+     alert('No cumples con la edad necesaria, aún te faltan' + ' ' + (65 - edad)+ ' ' + "años para poder jubilarte" );
+     console.log('Sigue laburando');
     }else{
-        alert('No cumples con la edad necesaria para poder jubilarte');
-        console.log('Seguí laburando');
+        alert ('No se pudo realizar la consulta \nalgunos de los datos ingresados son incorrectos');
     }
 }else{
     alert ('No se pudo realizar la consulta \ntodos los campos deben completarse');
-    console.log('Sigue laburando');
+    
 }
